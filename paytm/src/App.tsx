9b0050cabf48f2939ad1932a85a1f1f5b41ae1e4
AKSHIT0305 <akshit.goel.03@gmail.com>
2025-06-25 11:24:00 +0530

    changes

--- paytm/src/App.tsx
+++ paytm/src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import Homepage from './components/Homepage';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Homepage/>} />
+          {/* <Route path='/movie' element={</>} />
+          <Route path="/" element={</>} />
+          <Route path="/reports" element={</>} />
+          <Route path="/users" element={</>} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
